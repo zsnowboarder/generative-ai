4bfa8893100e53649d19fa56ba2f068cf57e72d5
Jimmy :) <146484453+zsnowboarder@users.noreply.github.com>
2024-11-18 19:22:24 -0800

    Update eIM_AI.py

--- eIM_AI.py
+++ eIM_AI.py
@@ -99,10 +99,10 @@ You can ask me specifically on what naming conventions I was trained on and what
 #if button is clicked
 if st.button("Generate Response"):
     placeholder = st.empty()
-    placeholder.write("Please patient as it may take me a few seconds as this is a trial version........")
+    placeholder.write("Please be patient as it may take me a few seconds to generate a response with this trial version........")
     result = generate(instructions, new_data)
     placeholder.empty()
-    placeholder.write("With this proof of concept, it is possible to use AI to reduce the repetive tasks and put officers back on the road. I can help add entities and text pages based on the information extracted from the officer's narrative. The possibility are endless.")
+    placeholder.write("With this proof of concept, it is possible to use AI to reduce the repetive tasks and put officers back on the road. I can help add entities and text pages based on the information extracted from the officer's narrative. The possibilities are endless.")
     st.text_area("Response", result, height=800)
 
 download_xml_button()
